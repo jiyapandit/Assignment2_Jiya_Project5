6fe5f7d25f9ab0b00dc68a97ab5bf19cb3150c5a
jiya <jpandit5253@conestogac.on.ca>
2024-09-18 17:58:05 -0400

    wrote the code which takes the data from the file and later we display it

--- Assignment2_Jiya_Project5/Source.cpp
+++ Assignment2_Jiya_Project5/Source.cpp
@@ -1,12 +1,77 @@
 //Jiya Pandit
-//Assignment 2
+//Assignment2
 
 
-// Creating a basic function vto just return 1
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+
+
+//Creating the struct
+
+struct STUDENT_DATA
+{
+    std::string FirstNameOfStudent;
+
+    std::string LastNameOfStudent;
+};
 
 int main()
-
 {
-	return 1;
+   
+    std::ifstream file("StudentData.txt");  // to open this file
+
+    std::string line;
+
+    std::vector<STUDENT_DATA> Students;
+
+  
+    if (file.is_open())
+    {
+       
+        for (; std::getline(file, line); )   //to get read and get data idf file is open
+        {
+            std::istringstream ss(line);
+
+            std::string FirstNameOfStudent, LastNameOfStudent;
+
+           
+            if (std::getline(ss, FirstNameOfStudent, ',') && std::getline(ss, LastNameOfStudent)) // to get the first and last name of student
+
+            {
+                
+                STUDENT_DATA Student{ FirstNameOfStudent, LastNameOfStudent };
+
+                Students.push_back(Student);
+            }
+
+        }
+
+        file.close();   //this is for closing the file
+    }
+
+    else
+
+    {
+        std::cerr << "Sorry, we are not able to open the file!!!" << std::endl;  // printing incase the file is not opened
+
+        return 1;  
+    }
+
+    int counter = 1; // counter for numbering of students
+  
+    for (const auto& Student : Students)
+    {
+        //output displatying thenames of teh students
+
+        std::cout << counter <<" First Name:- " << Student.FirstNameOfStudent<< ", Last Name:- " << Student.LastNameOfStudent << std::endl;
+
+        counter++;
+        
+    }
+
+    return 0;
 
 }
