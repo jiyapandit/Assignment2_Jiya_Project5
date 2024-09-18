57dff6f1ca6826dccb9f86fdb00886db21f3e53a
jiya <jpandit5253@conestogac.on.ca>
2024-09-18 18:19:17 -0400

    added the prerealease mode which also prints the email addresses

--- Assignment2_Jiya_Project5/Source.cpp
+++ Assignment2_Jiya_Project5/Source.cpp
@@ -18,12 +18,25 @@ struct STUDENT_DATA
     std::string FirstNameOfStudent;
 
     std::string LastNameOfStudent;
+
+    std::string EmailAddressOfStudent;
 };
 
 int main()
 {
    
-    std::ifstream file("StudentData.txt");  // to open this file
+#ifdef PRE_RELEASE
+
+    std::cout << "Running in Pre-Release Mode" << std::endl;
+
+    std::ifstream file("StudentData_Emails.txt");  //This is because we want to use the email address of the students in the pre release mode
+#else
+
+    std::cout << "Running in Standard Mode" << std::endl;
+
+    std::ifstream file("StudentData.txt"); 
+
+#endif
 
     std::string line;
 
@@ -37,18 +50,24 @@ int main()
         {
             std::istringstream ss(line);
 
-            std::string FirstNameOfStudent, LastNameOfStudent;
+            std::string FirstNameOfStudent, LastNameOfStudent, EmailAddressOfStudent;
 
-           
-            if (std::getline(ss, FirstNameOfStudent, ',') && std::getline(ss, LastNameOfStudent)) // to get the first and last name of student
 
+#ifdef PRE_RELEASE
+
+            if (std::getline(ss, FirstNameOfStudent, ',') && std::getline(ss, LastNameOfStudent, ',') && std::getline(ss, EmailAddressOfStudent))
+            
             {
-                
-                STUDENT_DATA Student{ FirstNameOfStudent, LastNameOfStudent };
-
+                STUDENT_DATA Student{ FirstNameOfStudent, LastNameOfStudent, EmailAddressOfStudent };  // Include email address of student in the  pre-release
+#else
+            if (std::getline(ss, FirstNameOfStudent, ',') && std::getline(ss, LastNameOfStudent))
+            
+            {
+                STUDENT_DATA Student{ FirstNameOfStudent, LastNameOfStudent, "" };  // No email address in standard mode
+#endif
                 Students.push_back(Student);
-            }
 
+            }
         }
 
         file.close();   //this is for closing the file
@@ -62,7 +81,7 @@ int main()
         return 1;  
     }
 
-#ifdef _DEBUG 
+
 
     int counter = 1; // counter for numbering of students
   
@@ -70,12 +89,22 @@ int main()
     {
         //output displatying thenames of teh students
 
-        std::cout << counter <<" First Name:- " << Student.FirstNameOfStudent<< ", Last Name:- " << Student.LastNameOfStudent << std::endl;
+        std::cout << counter <<" First Name:- " << Student.FirstNameOfStudent<< " Last Name:- " << Student.LastNameOfStudent << std::endl;
+
+        std::cout << std::endl;
+
+#ifdef PRE_RELEASE
+
+        std::cout << " Email: " << Student.EmailAddressOfStudent;  // Print email only in pre-release mode
+
+        std::cout << std::endl;
+#endif
+        std::cout << std::endl;
 
         counter++;
         
     }
-#endif
+
 
     return 0;
 
